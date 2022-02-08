331e70ccab4771a96d925b0287bfd0d66c440b28
Phan Lê Min <mintn1k35@gmail.com>
2022-02-08 14:57:27 +0700

    husky

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const { extname } = require('path');
 const app = express();
 const port = 3000;
 
-          app.use(express.static(path.join(__dirname, 'public')));
+            app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
     express.urlencoded({
